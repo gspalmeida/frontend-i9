cd6677f3013208a8e86b1a325733a1e70ed73651
Gustavo Almeida <gustavo@cuideme.care>
2021-02-01 15:20:19 -0300

    API do login com o backend

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,31 @@
 import React, { FormEvent, useState } from "react";
+
+import { useAuth } from '../../hooks/auth';
+
 import CardForm from "../../components/CardForm";
 
 import { InputBlock, Input } from "./styles";
+import { useHistory } from "react-router-dom";
 
 const Login: React.FC = () => {
+  const history = useHistory();
+
   const [email, SetEmail] = useState("");
   const [password, SetPassword] = useState("");
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-
-    const data = {
-      email,
-      password,
-    };
+  
+  const { signIn } = useAuth();
+  
+  const handleSubmit = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+      await signIn({
+        email,
+        password
+      });
+      history.push('/home');
+    } catch (error) {
+      alert("Não foi possível realizar  o LogIn, entre em contato com o administrador: \n\n Gustavo - (44) 9 9957-1618")
+    }
   };
   return (
     <CardForm
