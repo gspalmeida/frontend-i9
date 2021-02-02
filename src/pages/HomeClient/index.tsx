0648f167dc3f5f325002837c5fee2b50e5dd0e42
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-02-02 01:50:52 -0300

    listagem de serviços

--- src/pages/HomeClient/index.tsx
+++ src/pages/HomeClient/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FilterByDate from "../../components/FilterByDate";
 import Modal from "../../components/Modal";
 import Table from "../../components/Table";
 import Header from "../../components/Header";
 
 import { Container, Title } from "./styles";
+import api from "../../services/api";
 
 interface IProductDetail {
   id: number;
@@ -41,6 +42,35 @@ const HomeClient: React.FC = () => {
   const [modalProductDetail, setModalProductDetail] = useState<IProductDetail>(
     {} as IProductDetail
   );
+
+  const getServices = async () => {
+    const { data } = await api.get("/services");
+
+    const servicesData = data.map((MyService: any) => {
+      const {
+        id,
+        name,
+        service: { service_name },
+        description,
+        value,
+        due_date,
+      } = MyService;
+      return {
+        id,
+        name,
+        tipo: service_name,
+        descricao: description,
+        valor: value,
+        disponivelAte: due_date,
+      };
+    });
+
+    setServices(servicesData);
+  };
+
+  useEffect(() => {
+    getServices();
+  }, []);
 
   return (
     <>
