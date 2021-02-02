513c3de4808f7bebf99dc912ccb13588b555a952
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-02-01 23:27:51 -0300

    adicionando dados reais no modal i adicionando funcionalidade de fechar o modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -4,15 +4,31 @@ import { useState } from "react";
 import DatePicker from "../DatePicker";
 import Select from "../Select";
 
-import { InputBlock, Input } from "./styles";
+import { CloseIcon, InputBlock, Input } from "./styles";
 
 import MenuItem from "@material-ui/core/MenuItem";
 import CardForm from "../CardForm";
 
-const Modal: React.FC = () => {
-  const [serviceName, setServiceName] = useState("");
-  const [description, setDescription] = useState("");
-  const [serviceValue, setServiceValue] = useState("");
+interface IModal {
+  productDetail: IProductDetail;
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface IProductDetail {
+  id: number;
+  name: string;
+  tipo: string;
+  descricao: string;
+  valor: string;
+  disponivelAte: string;
+}
+
+const Modal: React.FC<IModal> = ({ productDetail, setOpenModal }) => {
+  const [serviceName, setServiceName] = useState(productDetail.name);
+  const [description, setDescription] = useState(productDetail.descricao);
+  const [serviceValue, setServiceValue] = useState(
+    productDetail.valor.replace(",", ".")
+  );
 
   const [selectOpen, setSelectOpen] = useState(false);
   const [selectValue, setSelectValue] = useState<string | number>(1);
@@ -21,7 +37,9 @@ const Modal: React.FC = () => {
     { id: 2, name: "teste2" },
   ]);
 
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(
+    new Date(new Date(productDetail.disponivelAte).toLocaleDateString("pt-br"))
+  );
 
   const handleDateChange = (date: any) => {
     setSelectedDate(date);
@@ -32,7 +50,6 @@ const Modal: React.FC = () => {
   };
   const handleSelectOpen = () => {
     setSelectOpen(true);
-    setServiceTypes([]);
   };
 
   const handleSelectChange = (event: React.ChangeEvent<{ value: unknown }>) => {
@@ -42,74 +59,85 @@ const Modal: React.FC = () => {
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
-  return (
-    <CardForm
-      title={`Editar serviço`}
-      buttonTitle="Aplicar Alterações"
-      onSubmit={handleSubmit}
-      style={{
-        background: "rgba(0,0,0,0.85)",
-        position: "absolute",
-        top: 0,
-        left: 0,
-      }}
-    >
-      <InputBlock>
-        <Input
-          required
-          placeholder="Nome do serviço"
-          value={serviceName}
-          onChange={(e) => {
-            setServiceName(e.target.value);
-          }}
-        />
-        <Input
-          required
-          placeholder="Descrição"
-          as="textarea"
-          value={description}
-          style={{
-            height: 100,
-            paddingTop: 10,
-            maxHeight: 230,
-            minHeight: 40,
-            resize: "vertical",
-          }}
-          onChange={(e: any) => {
-            setDescription(e.target.value);
-          }}
-        />
-        <Input
-          required
-          placeholder="Valor"
-          type="number"
-          value={serviceValue}
-          onChange={(e) => {
-            setServiceValue(e.target.value);
-          }}
-        />
-        <Select
-          open={selectOpen}
-          value={selectValue}
-          handleClose={handleSelectClose}
-          handleOpen={handleSelectOpen}
-          handleChange={handleSelectChange}
-        >
-          {serviceTypes &&
-            serviceTypes.map((serviceType) => (
-              <MenuItem key={serviceType.id} value={serviceType.id}>
-                {serviceType.name}
-              </MenuItem>
-            ))}
-        </Select>
 
-        <DatePicker
-          value={selectedDate}
-          placeholder="Disponível até"
-          handleChange={handleDateChange}
-        />
-      </InputBlock>
-    </CardForm>
+  if (!productDetail) {
+    return null;
+  }
+  return (
+    <>
+      <CloseIcon
+        onClick={() => {
+          setOpenModal(false);
+        }}
+      />
+      <CardForm
+        title={`Editar serviço`}
+        buttonTitle="Aplicar Alterações"
+        onSubmit={handleSubmit}
+        style={{
+          background: "rgba(0,0,0,0.85)",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+      >
+        <InputBlock>
+          <Input
+            required
+            placeholder="Nome do serviço"
+            value={serviceName}
+            onChange={(e) => {
+              setServiceName(e.target.value);
+            }}
+          />
+          <Input
+            required
+            placeholder="Descrição"
+            as="textarea"
+            value={description}
+            style={{
+              height: 100,
+              paddingTop: 10,
+              maxHeight: 230,
+              minHeight: 40,
+              resize: "vertical",
+            }}
+            onChange={(e: any) => {
+              setDescription(e.target.value);
+            }}
+          />
+          <Input
+            required
+            placeholder="Valor"
+            type="number"
+            value={Number(serviceValue)}
+            onChange={(e) => {
+              setServiceValue(e.target.value);
+            }}
+          />
+          <Select
+            open={selectOpen}
+            value={selectValue}
+            handleClose={handleSelectClose}
+            handleOpen={handleSelectOpen}
+            handleChange={handleSelectChange}
+          >
+            {serviceTypes &&
+              serviceTypes.map((serviceType) => (
+                <MenuItem key={serviceType.id} value={serviceType.id}>
+                  {serviceType.name}
+                </MenuItem>
+              ))}
+          </Select>
+
+          <DatePicker
+            value={selectedDate}
+            placeholder="Disponível até"
+            handleChange={handleDateChange}
+          />
+        </InputBlock>
+      </CardForm>
+    </>
   );
 };
 
