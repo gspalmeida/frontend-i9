b2e3eb275948db63687796af489cfbb737ef13c4
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-02-02 01:50:07 -0300

    Header funcional(porem feio)

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { Link } from "react-router-dom";
+
 export const Container = styled.div`
   width: 100%;
   height: 60px;
@@ -10,8 +12,42 @@ export const Container = styled.div`
 
 export const Wrapper = styled.div`
   width: min(1200px, 100%);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-export const Left = styled.div``;
+export const Left = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-export const Right = styled.div``;
+export const NewServiceLink = styled(Link)`
+  margin-left: 30px;
+  color: #fff;
+  font-weight: 700;
+
+  text-decoration: none;
+  font-size: 16px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+export const Right = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Avatar = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+`;
+
+export const Username = styled.span`
+  margin-left: 15px;
+  color: #fff;
+  font-size: 15px;
+`;
