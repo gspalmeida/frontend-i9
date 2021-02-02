8446e63b1dd01f42f2859ad8eeb97266b110ef65
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-02-02 01:50:07 -0300

    Header funcional(porem feio)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 
-import { Container, Wrapper, Left, Right } from "./styles";
+import {
+  Container,
+  Wrapper,
+  Left,
+  NewServiceLink,
+  Right,
+  Avatar,
+  Username,
+} from "./styles";
 
 const Header: React.FC = () => {
   return (
     <Container>
       <Wrapper>
-        <Left></Left>
-        <Right></Right>
+        <Left>
+          <h1>Logo</h1>
+          <NewServiceLink to="/criarservico">Criar serviço</NewServiceLink>
+        </Left>
+        <Right>
+          <Avatar src={process.env.REACT_APP_SERVER_STATIC_URL} />
+          <Username>HeitorMaf</Username>
+        </Right>
       </Wrapper>
     </Container>
   );
