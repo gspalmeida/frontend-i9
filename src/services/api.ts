4331a9e99f9c7520a064a6f52242cf4e6ff8d855
Gustavo Almeida <gustavo@cuideme.care>
2021-02-05 07:56:54 -0300

    Adição de https ao axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import { BASEURL as baseURL } from '@env';
 
 const api = axios.create({
-  baseURL: 'http://18.229.244.224:1919',
+  baseURL: 'https://18.229.244.224:1919',
 });
 
 export default api;
